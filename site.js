90b56a8258c4c61a996f26f1df4b0b1839ecd62e
LabRatKali <labratkali@outlook.com>
2026-07-21 15:51:55 -0400

    site: update site.js

--- site.js
+++ site.js
@@ -3,7 +3,7 @@ async function loadLatest() {
   const apkLink = document.getElementById("apk-link");
   if (!versionLine || !apkLink) return;
 
-  const FALLBACK_VERSION = "v2.10.2";
+  const FALLBACK_VERSION = "v2.11.0";
   const template =
     versionLine.dataset.template || "Latest release: %s";
   const formatVersion = (ver) => template.replace("%s", ver);
